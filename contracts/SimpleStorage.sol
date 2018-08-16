a4a25b50ecddbab63f879e522e7f082203f7f420
xinghengwang <xing@moesif.com>
2018-08-15 17:31:51 -0700

    Update: add example for events.

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.4.18;
 contract SimpleStorage {
   uint storedData;
 
+  event Hello(string name);
+  event Deposit(address indexed from, uint value);
+
   function set(uint x) public {
     storedData = x;
+    emit Deposit(msg.sender, x);
+    emit Hello("world");
   }
 
   function get() public view returns (uint) {
